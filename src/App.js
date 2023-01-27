bc41a715376a1349845d7761ec4864e60df3f164
Phiwe <wycliffwhite7@gmail.com>
2023-01-27 16:28:54 -0500

    order Summary Component Files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import headerImg from './images/illustration-hero.svg';
+import icon from './images/icon-music.svg'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container card">
+      <img src={headerImg} alt="" className="" />
+      
+      <div class="card-content">
+        <h2 className="heading">Order Summary</h2>
+        <p class="desc">You can now listen to millions of songs, audio books, and podcasts on any device anywhere you like!</p>
+
+        <div className="pricing">
+          <img src={icon} className="music-icon" alt='img'/>
+          <div class="plan-details">
+            <h4 className="plan-title">Annual Plan</h4>
+            <h4 className="price">$59.99/year</h4>
+          </div>
+          <a href='google.com' className="change"> Change</a>
+        </div>
+
+        <button type="submit">Proceed to Payment</button>
+
+        <h4 className="cancel">Cancel Order</h4>    
+      </div>
+  </div> // End card container
   );
 }
 
